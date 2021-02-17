0b7f01a65df4586186fb3f6051a4e54513412982
Caitlin Cabrera <caitlin@Caitlins-MBP-2.attlocal.net>
2021-02-17 14:13:18 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from googlesearch import search
+
+#user menu
+print("What would you like to do?")
+print("Please enter a number")
+print("1 to search by name")
+print("2 to search by ip address")
+print("3 to search by specific file extension")
+choice = input("Enter your choice: ")
+
+if choice == 1:
+    query = input('Enter the name enclosed in double quotes like so: "name" ')
+    stopNum = input("How many results would you like to see? ")
+    f = open("companyNames.txt", "w+")
+    for url in search(query, stop=stopNum):
+        print(url)
+        f.write(url + "\n")
+    print("Results written to companyNames.txt")
+
+if choice == 2:
+    query = input('Enter the ip address enclosed in double quotes like so: "000.000.000" ')
+    stopNum = input("How many results would you like to see? ")
+    f = open("ipAddresses.txt", "w+")
+    for url in search(query, stop=stopNum):
+        print(url)
+        f.write(url + "\n")
+    print("Results written to ipAddresses.txt")
+
+if choice == 3:
+    targetSite = input('Enter the target site ex: "redhat.com" ')
+    query = input('Enter the specific file extension ex: "txt" ')
+    stopNum = input("How many results would you like to see? ")
+    f = open("fileExtensions.txt", "w+")
+    for url in search("site: <"+targetSite+"> inurl:php."+query+" filetype:" + query, stop=stopNum):
+        print(url)
+        f.write(url + "\n")
+    print("Results written to fileExtensions.txt")
+
